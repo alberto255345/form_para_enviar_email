46ec9eebcc99330e41dd698fe6a899d771adbec7
alberto255345 <alberto255345@users.noreply.github.com>
2020-08-27 14:57:01 -0300

    correção

--- index.php
+++ index.php
@@ -10,22 +10,22 @@
 <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
 </header>
 <body>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+<script src="jquery-3.5.1.js"></script>
   <script src="notify.js"></script>
 <div class="card amianto w3-animate-top">
 <h1>Parabéns!</h1>
 <p>Agora falta muito pouco para você receber seu super presente...</p>
-<form>
+<div id="form">
 <p>Dados para agendamento:</p>
 <div class="form-group">
     <label for="exampleInputEmail1" class="bmd-label-floating">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1">
+    <input name="nameinput" type="email" class="form-control" id="exampleInputEmail1">
     <span class="bmd-help">Insira aqui o seu endereço de e-mail.</span>
 </div>
 <p>Data para entrega:</p>
 <div class="radio">
     <label>
-      <input type="radio" name="optionsRadios" id="optionsRadios1" value="31/08/2020" checked>31/08/2020
+      <input type="radio" name="optionsRadios" id="optionsRadios1" value="31/08/2020">31/08/2020
     </label>
   </div>
   <div class="radio">
@@ -33,12 +33,40 @@
       <input type="radio" name="optionsRadios" id="optionsRadios2" value="01/09/2020">01/09/2020
     </label>
   </div>
-  <button type="submit" class="btn btn-primary btn-raised">Enviar</button>
-</form>
+  <button id="target" type="submit" class="btn btn-primary btn-raised">Enviar</button>
 </div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+</div>
 <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
-<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+<script>
+$(document).ready(function() { 
+    $('body').bootstrapMaterialDesign(); 
+    
+    function isEmail(email) {
+    var check = "" + email;
+    if((check.search('@')>=0)&&(check.search(/\./)>=0))
+        if(check.search('@')<check.split('@')[1].search(/\./)+check.search('@')) return true;
+        else return false;
+    else return false;
+}
+
+    $( "#target" ).click(function() {
+        if ( isEmail( $("#exampleInputEmail1").val()) ) {
+            $.ajax({
+                method: "POST",
+                url: "send.php",
+                data: { email: $("#exampleInputEmail1").val(), dia:  $("input[name=optionsRadios]:checked").val() }
+            })
+            .done(function( msg ) {
+                $.notify(msg);
+            });
+
+        }else{
+            alert( "Não é um e-mail valido", "info" );
+        }
+    });
+
+
+    });</script>
 </body>
 </html>
--- send.php
+++ send.php
@@ -10,23 +10,35 @@ use PHPMailer\PHPMailer\Exception;
 $mail = new PHPMailer(true);
  
 try {
-	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    
+	//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 	$mail->isSMTP();
 	$mail->Host = 'smtp.gmail.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = 'alberto.neto@edu.unifor.br';
+    $mail->CharSet = "utf-8";// set charset to utf8
+    $mail->SMTPAuth = true;// Enable SMTP authentication
+    $mail->SMTPSecure = 'tls';// Enable TLS encryption, `ssl` also accepted
+    
+    $mail->Username = 'alberto.neto@edu.unifor.br';
 	$mail->Password = '15069400';
 	$mail->Port = 587;
- 
-	$mail->setFrom('alberto.neto@edu.unifor.br');
-	$mail->addAddress('b.nobre@hotmail.com');
-	$mail->addAddress('alberto.neto94@gmail.com');
- 
+    
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+	$mail->setFrom('alberto.neto@edu.unifor.br', 'Alberto Vitoriano');
+	$mail->addAddress($_POST["email"]);
+	$mail->AddBCC('alberto.neto94@gmail.com');
+    
 	$mail->isHTML(true);
-	$mail->Subject = 'Teste de email via gmail Canal TI';
-	$mail->Body = 'Chegou o email teste do <strong>Canal TI</strong>';
-	$mail->AltBody = 'Chegou o email teste do Canal TI';
- 
+	$mail->Subject = 'Escolha da data do Presente!';
+	$mail->Body = 'Olá, você acessou o site e escolheu o recebimento do presente para o dia <strong>' . $_POST["dia"] . '</strong>. Em breve chegara na sua casa!';
+	$mail->AltBody = 'Parabéns pela a escolha do Presente!';
+    
 	if($mail->send()) {
 		echo 'Email enviado com sucesso';
 	} else {
